c83cffdead4d6960b725aded14034d64327d2c17
AkhatKhusainov <aleksej.leha2212@gmail.com>
2021-10-02 15:04:43 +0300

    <Required>

---
+++ Задание1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	if (a >= 100 && a <= 999) {
+		cout << "all right";
+	}
+	else {
+		cout << "NO";
+	}
+}
---
+++ Задание2_новый_год.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int d;
+	int m;
+	int s;
+	cout << "введите день" << endl;
+	cin >> d;
+	cout << "введите мес€ц" << endl;
+	cin >> m;
+	if (m <= 12 && d <= 31) {
+		if (m == 1) {
+			s = d;
+		}
+		else {
+			if (m == 2) {
+				s = (m - 1) * 31 + d;
+			}if (m > 2 && m <= 8) {
+				s = (m - 1) * 31 - 3 - ((m - 3) / 2) + d;
+			}if (m > 8) {
+				s = (m - 1) * 31 - 3 - ((m - 4) / 2) + d;
+			}
+		}
+		s = 365 - s;
+		cout << s;
+	}
+	else {
+		cout << "error";
+	}
+
+}
